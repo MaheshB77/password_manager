9e452269acead060ff7b70328f54a54bc69e29b9
MaheshB77 <mbansode7696@gmail.com>
2024-05-06 20:51:51 +0530

    Added icons to the password list

---
+++ lib/screens/home_screen/widgets/password_avatar.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:password_manager/constants/icons.dart';
+import 'package:password_manager/models/password.dart';
+import 'package:password_manager/utils/icon_util.dart';
+
+class PasswordAvatar extends StatelessWidget {
+  final Password password;
+  const PasswordAvatar({super.key, required this.password});
+
+  @override
+  Widget build(BuildContext context) {
+    return password.iconId == null
+        ? Image.asset(
+            IconUtil.defaultIcon.url,
+            width: 40,
+          )
+        : Image.asset(
+            IconUtil.getById(pmIcons, password.iconId!).url,
+            width: 40,
+          );
+  }
+}
--- lib/screens/home_screen/widgets/password_tile.dart
+++ lib/screens/home_screen/widgets/password_tile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:password_manager/models/category.dart';
 import 'package:password_manager/models/password.dart';
+import 'package:password_manager/screens/home_screen/widgets/password_avatar.dart';
 
 class PasswordTile extends ConsumerWidget {
   final Password password;
@@ -23,9 +24,7 @@ class PasswordTile extends ConsumerWidget {
     return Container(
       margin: const EdgeInsets.only(bottom: 4),
       child: ListTile(
-        leading: CircleAvatar(
-          child: Text(password.title[0].toUpperCase()),
-        ),
+        leading: PasswordAvatar(password: password),
         title: Text(
           password.title,
           style: Theme.of(context)
