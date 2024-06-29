d80c2fc3db7032ed820d9f04826457e23f4ab6eb
MaheshB77 <mbansode7696@gmail.com>
2024-06-29 16:47:53 +0530

    Added sign-in robot

---
+++ integration_test/robot/signin_robot.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:password_manager/constants/keys.dart';
+import 'package:password_manager/screens/login_screen/widgets/button.dart';
+import 'package:password_manager/screens/login_screen/widgets/signin_form.dart';
+import 'package:password_manager/screens/passwords_screen/passwords_screen.dart';
+import 'package:password_manager/main.dart' as app;
+
+import '../utils/test_utils.dart';
+
+class SignUpRobot {
+  final WidgetTester tester;
+
+  SignUpRobot(this.tester);
+
+  Future<void> signIn() async {
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: app.MyApp(),
+      ),
+    );
+    await tester.pumpAndSettle();
+
+    await tester.enterText(
+      find.byKey(AppKeys.signInPassword),
+      TestConstants.password,
+    );
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.byType(Button));
+    await tester.pumpAndSettle();
+
+    expect(find.byType(SignInForm), findsNothing);
+    expect(find.byType(PasswordsScreen), findsOne);
+  }
+}
